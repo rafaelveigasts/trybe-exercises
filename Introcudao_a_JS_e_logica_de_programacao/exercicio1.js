bb69a8d0b01ee0d03e3c4a9432c7923d0271a796
Rafael Veiga <rafaelveig@hotmail.com>
2021-06-23 16:10:21 -0300

    adicionando readme para novo conteúdo

---
+++ Introcudao_a_JS_e_logica_de_programacao/exercicio1.js
@@ -0,0 +1,10 @@
+const nome = "rafael";
+const birthCity = "guarujá"
+const birthYear = "2030";
+
+
+console.log(nome);
+console.log(birthCity);
+console.log(birthYear);
+
+//const não pode ser reatribuído um novo valor.
---
+++ Introcudao_a_JS_e_logica_de_programacao/exercicio2.js
@@ -0,0 +1,9 @@
+let base = 5
+let altura = 8
+let area = base*altura
+
+console.log(area)
+
+let perimetro = 8*2+5*2
+
+console.log(perimetro)
