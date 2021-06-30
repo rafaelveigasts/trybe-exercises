164e4ef25792021f0fde71ee0d316d5b3312df35
Rafael Veiga <rafaelveig@hotmail.com>
2021-06-30 19:31:27 -0300

    exercicios querrySelector

---
+++ bloco5/exercicios/script.js
@@ -0,0 +1,19 @@
+document.querySelector("#header-container").style.backgroundColor = "green";
+document.querySelector("h1").style.color = "white";
+
+document.querySelector("h1").style.color = "white";
+
+
+let bgEmergencyTasks =  document.querySelectorAll('.emergency-tasks h3')
+  for (let i = 0; i < bgEmergencyTasks.length; i++){
+    bgEmergencyTasks[i].style.backgroundColor = 'blue'
+  }
+
+document.querySelector('.emergency-tasks').style.backgroundColor="pink";
+
+let bgNoEmergencyTasks =  document.querySelectorAll('.no-emergency-tasks h3')
+  for (let i = 0; i < bgNoEmergencyTasks.length; i++){
+    bgNoEmergencyTasks[i].style.backgroundColor = 'black'
+  }
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor="yellow";
