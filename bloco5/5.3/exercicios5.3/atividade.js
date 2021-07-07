94f96e5a81960309297da8875a03294f5599952f
Rafael Veiga <rafaelveig@hotmail.com>
2021-07-06 21:47:57 -0300

    exercicio atualizado e inicio atividade

---
+++ bloco5/5.3/exercicios5.3/atividade.js
@@ -0,0 +1,50 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+/*crie dinamicamente cada dia do calendáriocada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>*/
+
+let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfTheMonth() {
+  let getDaysList = document.querySelector('#days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let day = dezDaysList[index];
+    let dayItem = document.createElement('li');
+
+    if (day === 24 | day === 25 | day === 31) {
+      dayItem.className = 'holiday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);}
+    else if (day === 4 | day === 11 | day === 25) {
+      dayItem.className = 'day friday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);}
+      if (day === 25 ) {
+        dayItem.className = 'holiday day friday';
+        dayItem.innerHTML = day;
+        getDaysList.appendChild(dayItem);}
+        else{
+          dayItem.innerHTML = day;
+          dayItem.className = 'day'
+          getDaysList.appendChild(dayItem);
+        }
+  }
+}
