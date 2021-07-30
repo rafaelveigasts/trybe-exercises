ea53b62d84aa5da2dc156f74f7556e9ae933ea2c
Rafael Veiga <rafaelveig@hotmail.com>
2021-07-30 15:00:15 -0300

    atividade 1 e 2 array filter e map

---
+++ bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/Atividade2FiltarMap.js
@@ -0,0 +1,39 @@
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// Dica: use as funções map , sort
+
+function nameAndAge() {
+  expectedResult.sort((a,b)=> a.age - b.age)
+  return expectedResult
+}
+
+console.log(nameAndAge())
+
+
+// assert.deepStrictEqual(nameAndAge(), expectedResult);
