a8d9309c1a2f48adaed253c8e61e9580bcb0e980
Rafael Veiga <rafaelveig@hotmail.com>
2021-08-11 10:24:05 -0300

    atividade 4.js

---
+++ bloco-9-javascript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/atividade4.js
@@ -0,0 +1,16 @@
+// 4 - Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature , que imprime a temperatura em Marte.
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const sendMarsTemperature = () => {
+  setTimeout(() => console.log('Mars temperature is 20 graus Celcius'),(Math.random()*5)+1);
+
+  // setTimeout(()=> console.log("Returned planet: ", mars), 4000);
+
+}
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
