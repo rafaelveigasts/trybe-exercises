5909a0179f51f38d41f1de2d0f3f18ae032fd291
Rafael Veiga <85757603+rafaelveigasts@users.noreply.github.com>
2021-08-18 16:16:26 -0300

    Delete introducao-a-javascript-e-logica-de-programacao.2 directory

--- introducao-a-javascript-e-logica-de-programacao.2/exercicioarray2.js
+++
@@ -1,7 +0,0 @@
-//Procure o índice do valor "Portfólio" do array menu :
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf('Portfólio');
-
-console.log(indexOfPortfolio);
-
