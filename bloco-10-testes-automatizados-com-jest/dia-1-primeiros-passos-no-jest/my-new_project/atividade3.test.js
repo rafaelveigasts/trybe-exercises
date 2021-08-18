1eced70a6132c9dc9accf0ad9663a83eb0345405
Rafael Veiga <rafaelveig@hotmail.com>
2021-08-18 11:55:28 -0300

    exercicios completos 1 ao 5 refatorados

--- bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/my-new_project/atividade3.test.js
+++ bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/my-new_project/atividade3.test.js
@@ -33,9 +33,9 @@ describe('Revalida as funções do exercício 3 dos testes unitários', ()=>
   test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () =>{
     expect(myRemoveWithoutCopy([1,2,3,4],3)).not.toEqual([1,2,3,4])
   })
-  // test('Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações', () => {
-  //   expect(myRemoveWithoutCopy([1,2,3,4])).toBe('array')
-  // })
+  test('Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações', () => {
+    expect(myRemoveWithoutCopy([1,2,3,4])).toEqual([1,2,3,4])
+  })
   test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
     expect(myRemoveWithoutCopy([1,2,3,4], 5)).toEqual([1,2,3,4])
   })
