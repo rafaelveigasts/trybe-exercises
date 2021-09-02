8482e56c6149db8c9fd27ea3a5593569ebfcf284
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-02 15:54:50 -0300

    intro this and bind

--- bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,27 +1,64 @@
 import React from 'react';
 
-function handleClickA(){
-  console.log('pega na minha')
-}
-
-function handleClickB(){
-  console.log('balança mas não para')
-}
-
-function handleClickC(){
-  console.log('kkkkkkkkkkkkk')
-}
 
 class App extends React.Component{
+
+  constructor(){
+    super()
+    console.log('é só uma brincadeira')
+    this.handleClickA = this.handleClickA.bind(this)
+    this.handleClickB = this.handleClickB.bind(this)
+    this.handleClickC = this.handleClickC.bind(this)
+  }
+
+  handleClickA(){
+    console.log(this)
+    console.log('pega na minha')
+  }
+  
+   handleClickB(){
+    console.log(this)
+    console.log('balança mas não para')
+  }
+  
+  handleClickC(){
+    console.log(this)
+    console.log('kkkkkkkkkkkkk')
+  }
+
   render(){
+    console.log(this)
   return (
     <div>
-      <button onClick={handleClickA}>Btn A</button>
-      <button onClick={handleClickB}>Btn B</button>
-      <button onClick={handleClickC}>Btn C</button>
+      <button onClick={this.handleClickA}>Btn A</button>
+      <button onClick={this.handleClickB}>Btn B</button>
+      <button onClick={this.handleClickC}>Btn C</button>
     </div>
     );
   }
 }
 
 export default App;
+
+/*
+Quando colocamos uma funcao dentro de uma classe temos que tirar o function
+se deixarmos onclick={nomedafuncao} o js procura fora da classe
+para acessarmos a função dentro da classe usamos this.nomedafuncao
+
+A função constructor é uma função interna
+nós podemos sobreescrever essa função com uma função nossa
+
+super() serve para que o react  execute a lógica que estamos definindo
+sobreescrevendo a função interna e abaixo do super colocamos a logica que agente quiser
+
+o this tem um problema
+dentro do render o this funciona
+dentro da função ñao
+para arrumar isso fazemos o processo da linhas 9-11
+fazemos isso para a função enxergar o this
+'esta função está ligada ao this'
+<<<ATENÇÃO>>>
+se quiser usar o this.props tem que fazer isso
+para acessar o estado a função tem que acessar o this também
+
+*/
