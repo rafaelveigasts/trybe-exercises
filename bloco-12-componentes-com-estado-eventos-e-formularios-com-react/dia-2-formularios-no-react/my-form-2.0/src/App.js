84fdba8dde8e113f58b7e2310e4999a9024b73d7
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-04 12:20:21 -0300

    exercicio formulario 2

---
+++ bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Forms from "./Components/Form";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Forms />
+      </div>
+    );
+  }
+}
+
+export default App;
