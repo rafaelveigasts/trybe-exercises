f9f8b696379a5dac55cf0b43d8318e17b70780b3
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-27 13:36:30 -0300

    exemplo 1

--- bloco-15-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/aula2/src/App.test.js
+++ bloco-15-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/aula2/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(() => jest.clearAllMocks());
+it('fetch joke', async () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  jest.spyOn(global, "fetch")
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+  const { findByText } = render(<App />);
+  await findByText('Whiteboards ... are remarkable.');
+  expect(global.fetch).toBeCalledTimes(1);
+  expect(global.fetch).toBeCalledWith(
+    'https://icanhazdadjoke.com/',
+    {"headers": {"Accept": "application/json"}}
+  );
 });
