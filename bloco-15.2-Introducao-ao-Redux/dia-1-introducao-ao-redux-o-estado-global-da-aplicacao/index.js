9dc1cbb8be9379006ac9583e08f0830eb8af49d6
Rafael Veiga <rafaelveig@hotmail.com>
2021-10-07 09:55:47 -0300

    exercicio 1 completo

---
+++ bloco-15.2-Introducao-ao-Redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/index.js
@@ -0,0 +1,11 @@
+// Arquivo index.js
+
+import { combineReducers } from 'redux';
+import meuReducer from './meuReducer';
+import meuOutroReducer from './meuOutroReducer';
+
+const reducerCombinado = combineReducers({
+meuReducer,
+meuOutroReducer});
+
+export default reducerCombinado;
