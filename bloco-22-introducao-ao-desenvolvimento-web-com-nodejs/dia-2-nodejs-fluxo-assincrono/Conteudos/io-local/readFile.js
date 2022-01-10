54c6a36c0e6ea0c8c7f90a454caf36f56a878abe
rafael veiga <rafaelveig@hotmail.com>
2022-01-10 15:58:54 -0300

    atributos adicionais e metodos de leitura e escrita no fs

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/Conteudos/io-local/readFile.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/Conteudos/io-local/readFile.js
@@ -1,12 +1,11 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const nomeDoArquivo = 'meu-arquivo.txt';
 
-fs.readFile(nomeDoArquivo, 'utf8')
-  .then((data) => {
-    console.log(`Conteúdo do arquivo: ${data}`);
-  })
-  .catch((err) => {
+fs.readFile(nomeDoArquivo, 'utf8', (err, data) => {
+  if (err) {
     console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
-    process.exit(1); // Encerra a execução do script e informa ao sistema operacional que houve um erro com código
-  });
+    process.exit(1);
+  }
+  console.log(`Conteúdo do arquivo: ${data}`);
+});
