312ee3155bb3b17611cf7ba4535c13346ada697c
rafael veiga <rafaelveig@hotmail.com>
2022-01-11 15:28:06 -0300

    executando o teste

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/Conteudos/Exemplos/testes/testecalculasituacao.test.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/Conteudos/Exemplos/testes/testecalculasituacao.test.js
@@ -2,7 +2,7 @@ const {expect} = require('chai');
 
 const calculaSituacao = require('../calculaSituacao');
 
-describe('Quando a média for menor que 7', () => {
+Describe('Quando a média for menor que 7', () => {
   it('retorna "reprovado"', () => {
     const resposta = calculaSituacao(4);
 
