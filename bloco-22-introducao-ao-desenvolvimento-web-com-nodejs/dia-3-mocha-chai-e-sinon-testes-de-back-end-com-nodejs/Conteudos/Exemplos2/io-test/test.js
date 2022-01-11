fe76402d5e054527bd68932dae5e815eb74a08d7
rafael veiga <rafaelveig@hotmail.com>
2022-01-11 16:15:30 -0300

    stub

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/Conteudos/Exemplos2/io-test/test.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/Conteudos/Exemplos2/io-test/test.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const sinon = require('sinon');
 const { expect } = require('chai');
 
 const leArquivo = require('./leArquivo');
@@ -6,24 +8,46 @@ const CONTEUDO_DO_ARQUIVO = 'VQV com TDD';
 
 describe('leArquivo', () => {
   describe('Quando o arquivo existe', () => {
-    describe('a resposta', () => {
-      const resposta = leArquivo('arquivo.txt');
+    before(() => {
+      sinon.stub(fs, 'readFileSync').returns(CONTEUDO_DO_ARQUIVO);
+    });
 
+    after(() => {
+      fs.readFileSync.restore();
+    });
+
+    describe('a resposta', () => {
       it('é uma string', () => {
+        const resposta = leArquivo('arquivo.txt');
+
         expect(resposta).to.be.a('string');
       });
 
       it('é igual ao conteúdo do arquivo', () => {
+        const resposta = leArquivo('arquivo.txt');
+
         expect(resposta).to.be.equals(CONTEUDO_DO_ARQUIVO);
       });
     });
   });
 
   describe('Quando o arquivo não existe', () => {
-    it('a resposta é igual a "null"', () => {
-      const resposta = leArquivo('arquivo_que_nao_existe.txt');
+    before(() => {
+      sinon
+        .stub(fs, 'readFileSync')
+        .throws(new Error('Arquivo não encontrado'));
+    });
 
-      expect(resposta).to.be.equal(null);
+    after(() => {
+      fs.readFileSync.restore();
+    });
+
+    describe('a resposta', () => {
+      it('é igual a "null"', () => {
+        const resposta = leArquivo('arquivo_que_nao_existe.txt');
+
+        expect(resposta).to.be.equal(null);
+      });
     });
   });
 });
