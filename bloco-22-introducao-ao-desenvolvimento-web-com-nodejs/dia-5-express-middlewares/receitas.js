c11fa335635b9e5f64b23fb034fa856abae5f7c2
rafael veiga <rafaelveig@hotmail.com>
2022-01-13 18:24:04 -0300

    passando valores entre middlewares com objeto req

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/receitas.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/receitas.js
@@ -45,7 +45,8 @@ function validateData(req, res, next){
 
 app.post('/recipes', validateData, function (req,res) {
   const { id, name, price, waitTime } = req.body;
-  recipes.push({ id, name, price, waitTime});
+  const { username } = req.user;
+  recipes.push({ id, name, price, waitTime, chef: username });
   res.status(201).json({ message: 'Recipe created successfully!'});
 });
 
