a189dad9add52abb83d712e9e92d103c1220417c
rafael veiga <rafaelveig@hotmail.com>
2022-01-20 14:52:10 -0300

    exemplo pratico passo a passo

---
+++ bloco-23-Nodejs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/Conteudos/hello-msc/models/connection.js
@@ -0,0 +1,12 @@
+// hello-msc/models/connection.js
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'rafael',
+  password: '11463250',
+  database: 'model_example'
+});
+
+module.exports = connection;
