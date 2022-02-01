38d67891a9f275908126f01a95ede77f00e6572e
rafael veiga <rafaelveig@hotmail.com>
2022-02-01 17:22:28 -0300

    assossiations and attributes

--- bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/index.js
+++ bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/index.js
@@ -1,36 +1,54 @@
 // index.js
-const express = require('express');
-const { Address, Employee } = require('./models');
+const express = require("express");
+const { Address, Employee } = require("./models");
+const { Book, User } = require("./models");
 
 const app = express();
 
-app.get('/employees', async (_req, res) => {
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+app.get("/employees", async (_req, res) => {
   try {
     const employees = await Employee.findAll({
-      include: { model: Address, as: 'addresses' },
+      include: { model: Address, as: "addresses" },
     });
 
     return res.status(200).json(employees);
   } catch (e) {
     console.log(e.message);
-    res.status(500).json({ message: 'Ocorreu um erro' });
-  };
+    res.status(500).json({ message: "Ocorreu um erro" });
+  }
 });
 
-app.get('/employees/:id', async (req, res) => {
+app.get("/employees/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const employee = await Employee.findOne({ where: { id } });
 
-    if (req.query.includeAddresses === 'true') {
+    if (req.query.includeAddresses === "true") {
       const addresses = await Address.findAll({ where: { employeeId: id } });
       return res.status(200).json({ employee, addresses });
     }
-
   } catch (e) {
     console.log(e.message);
-    res.status(500).json({ message: 'Algo deu errado' });
-  };
+    res.status(500).json({ message: "Algo deu errado" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
