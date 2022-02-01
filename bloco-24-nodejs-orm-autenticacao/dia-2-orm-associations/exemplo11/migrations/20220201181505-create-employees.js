8c07d2808b6ffbb53bd52965eb151218cd0f4db7
rafael veiga <rafaelveig@hotmail.com>
2022-02-01 15:16:47 -0300

    altera  migrations

--- bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/migrations/20220201181505-create-employees.js
+++ bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/migrations/20220201181505-create-employees.js
@@ -1,21 +1,30 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'first_name',
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'last_name',
+      },
+      age: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Employees');
+  },
 };
