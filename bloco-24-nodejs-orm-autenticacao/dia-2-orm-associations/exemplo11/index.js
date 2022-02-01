e4dba0c62d36ede642dfe727b3ee433408cb2d50
rafael veiga <rafaelveig@hotmail.com>
2022-02-01 16:49:21 -0300

    exemplo de lazy loading no index.js

--- bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/index.js
+++ bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/index.js
@@ -20,17 +20,13 @@ app.get('/employees', async (_req, res) => {
 app.get('/employees/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const employee = await Employee.findOne({
-        where: { id },
-        include: [{
-          model: Address, as: 'addresses', attributes: { exclude: ['number'] }, }]
-        // include: [{ model: Address, as: 'addresses' }],
-      });
+   const employee = await Employee.findOne({ where: { id } });
 
-    if (!employee)
-      return res.status(404).json({ message: 'Funcionário não encontrado' });
+   if (req.query.includeAddresses === 'true') {
+     const addresses = await Address.findAll({ where: { employeeId: id } });
+     return res.status(200).json(employee);
+   }
 
-    return res.status(200).json(employee);
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
