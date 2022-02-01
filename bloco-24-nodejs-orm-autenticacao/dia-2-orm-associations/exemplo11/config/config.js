89af7e46e62c6aa0298f79cf4f4e61ef6cfb82e4
rafael veiga <rafaelveig@hotmail.com>
2022-02-01 17:44:29 -0300

    post com unmanaged transactions

--- bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/config/config.js
+++ bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/config/config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "development": {
     "username": "rafael",
     "password": "Rafael12345!",
--- bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/index.js
+++ bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo11/index.js
@@ -2,10 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Address, Employee } = require("./models");
+const config = require('./config/config');
 const { Book, User } = require("./models");
 
 const app = express();
 app.use(bodyParser.json());
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(config.development);
 
 app.get('/usersbooks/:id', async (req, res) => {
   try {
@@ -39,17 +43,34 @@ app.get("/employees", async (_req, res) => {
 });
 
 app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
   try {
     const { firstName, lastName, age, city, street, number } = req.body;
 
-    const employee = await Employee.create({ firstName, lastName, age });
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
 
-    await Address.create({ city, street, number, employeeId: employee.id });
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
 
     return res.status(201).json({ message: 'Cadastrado com sucesso' });
   } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
     console.log(e.message);
-    res.status(500).json({ message: 'Algo deu errado' });
+    res.status(500).json({ message: 'Algo deu errado, fazendo roolback' });
   }
 });
 
