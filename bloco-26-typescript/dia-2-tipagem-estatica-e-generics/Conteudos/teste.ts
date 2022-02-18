9933d6c75fb02dfac8fae08917aa0b2c8d82eea0
rafael veiga <rafaelveig@hotmail.com>
2022-02-18 19:24:27 -0300

    generics

--- bloco-26-typescript/dia-2-tipagem-estatica-e-generics/Conteudos/teste.ts
+++ bloco-26-typescript/dia-2-tipagem-estatica-e-generics/Conteudos/teste.ts
@@ -220,3 +220,29 @@ let airplane: Airplane = {
   maxSpeed: 805,
   minSpeed: 0
 };
+
+function identity<T, U> (value: T, message: U) : T {
+  console.log(message);
+  return value
+}
+
+let returnNumber = identity<number, string>(100, "Olá");
+let returnString = identity<string, string>("100", "Mundo");
+let returnBoolean = identity<boolean, string>(true, "Olá, Mundo!");
+
+
+class ProcessIdentity<T, U> {
+  _value: T;
+  _message: U;
+  constructor(value: T, message: U) {
+      this._value = value;
+      this._message = message;
+  }
+  getIdentity() : T {
+      console.log(this._message);
+      return this._value
+  }
+}
+
+let processor = new ProcessIdentity<number, string>(100, "Olá");
+processor.getIdentity();  // imprime "Olá" e retorna 100
