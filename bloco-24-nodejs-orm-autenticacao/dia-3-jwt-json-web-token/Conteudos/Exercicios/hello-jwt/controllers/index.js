d33a2873488cac55fd0181c0a054409bd8717d5b
rafael veiga <rafaelveig@hotmail.com>
2022-02-18 14:44:35 -0300

    introducao

--- bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/Conteudos/Exercicios/hello-jwt/controllers/index.js
+++ bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/Conteudos/Exercicios/hello-jwt/controllers/index.js
@@ -1,5 +1,13 @@
-const login = require('./login');
+const me = require("./me");
+// const ping = require('./ping');
+const login = require("./login");
+const topSecret = require("./topSecret");
+const signup = require("./signup");
 
 module.exports = {
+  //   ping,
+  me,
   login,
+  topSecret,
+  signup,
 };
--- bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/Conteudos/Exercicios/hello-jwt/controllers/login.js
+++ bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/Conteudos/Exercicios/hello-jwt/controllers/login.js
@@ -1,31 +1,32 @@
 // controllers/login.js
-const Joi = require('joi');
-const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET } = process.env;
 
-const validateBody = (body) =>
-  /* Utilizamos o Joi para validar o schema do body */
-  Joi.object({
-    username: Joi.string().min(5).alphanum().required(),
-    password: Joi.string().min(5).required(),
-  }).validate(body);
+const service = require('../services/User');
+const { validateCredentials } = require('./utils/validateCredentials');
 
 module.exports = async (req, res, next) => {
-  /* Construímos um schema do Joi */
-  const { error } = validateBody(req.body);
+  const { error } = validateCredentials(req.body);
 
   /* Caso ocorra erro na validação do Joi, passamos esse */
   /* erro para o express, que chamará nosso middleware de erro */
   if (error) return next(error);
 
-  const payload ={
-    username: req.body.username,
-    admin: FontFaceSetLoadEvent
-  };
+  const { username, password } = req.body;
 
-  const token = jwt.sign(payload, JWT_SECRET, {
-    expiresIn: '1h',
-  });
+  /* Pedimos para o service gerar o token */
+  const { error: serviceError, token } = await service.login(username, password);
+
+  /* Caso ocorra um erro do tipo `invalidCredentials`,
+     retornamos um novo erro com status `401 unauthorized` */
+  if (serviceError && serviceError.code === 'invalidCredentials') {
+    return next({ statusCode: 401, message: serviceError.message });
+  }
+
+  /* Caso haja qualquer outro erro, acionamos o middleware de erro para obter uma mensagem genérica */
+  if (serviceError) {
+    return next(serviceError);
+  }
+
+  /* Por fim, caso nenhum erro tenha ocorrido, retornamos o token */
   res.status(200).json({ token });
 };
