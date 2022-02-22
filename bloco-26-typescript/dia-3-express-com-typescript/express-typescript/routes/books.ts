68bee35a54f34ce952b16128883b4dbb5c068f79
rafael veiga <rafaelveig@hotmail.com>
2022-02-22 11:29:17 -0300

    editar um livro

--- bloco-26-typescript/dia-3-express-com-typescript/express-typescript/routes/books.ts
+++ bloco-26-typescript/dia-3-express-com-typescript/express-typescript/routes/books.ts
@@ -40,8 +40,23 @@ router.post("/books", validationBook, async (req: Request, res: Response) => {
   return res.status(StatusCode.CREATED).json(book);
 });
 
-router.put("/books/:isbn", (req: Request, res: Response) => {
+router.put("/books/:isbn", validationBook, async (req: Request, res: Response) => {
 
+  const { isbn } = req.params;
+
+  const editedBook: Book = req.body;
+
+  const books = await read();
+
+  const index = books.findIndex(book => book.isbn === isbn);
+
+  if (index === -1) return res.status(StatusCode.NOT_FOUND).send(NotFoundMessage);
+
+  books.splice(index, 1, editedBook);
+
+  await write(books);
+
+  return res.status(StatusCode.OK).json(editedBook);
 });
 
 router.delete("/books/:isbn", (req: Request, res: Response) => {
