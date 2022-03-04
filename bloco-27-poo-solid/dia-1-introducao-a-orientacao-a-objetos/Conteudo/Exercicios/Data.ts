329d1b864d84e2bfbea0c175625a47383d413f73
rafael veiga <rafaelveig@hotmail.com>
2022-03-04 14:01:09 -0300

    exercicio 6 correto

--- bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Conteudo/Exercicios/Data.ts
+++ bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Conteudo/Exercicios/Data.ts
@@ -1,132 +1,129 @@
+
 class Data {
   private _day: number;
   private _month: number;
   private _year: number;
 
   constructor(day: number, month: number, year: number) {
-    const dateString = `${day}/${month}/${year}`;
 
-    // verifica se a data é válida
-    //    if (new Date(dateString).getDate() !== day) {
-    //     day = 1;
-    //     month = 1;
-    //     year = 1900;
-    // }
+      const dateStr = `${year}-${month}-${day}`;
 
-    this._day = day;
-    this._month = month;
-    this._year = year;
+      if (!this.validateDate(day, month, year)) {
+          day = 1;
+          month = 1;
+          year = 1900;
+      }
+
+      this._day = day;
+      this._month = month;
+      this._year = year;
   }
 
-  get day() {
-    return this.day;
+
+  get day(): number {
+      return this._day
   }
+
   set day(value: number) {
-    if (value < 1 || value > 31) {
-      throw new Error("Dia inválido");
-    }
-    this.day = value;
+      this._day = value;
   }
 
-  get Month() {
-    return this.Month;
+  get month(): number {
+      return this._month
   }
 
-  set Month(value: number) {
-    if (value < 1 || value > 12) {
-      throw new Error("Mês inválido");
-    }
-    this.Month = value;
+  set month(value: number) {
+      this._month = value;
   }
 
-  get Year() {
-    return this.Year;
+  get year(): number {
+      return this._year
   }
 
-  set Year(value: number) {
-    if (value < 1900) {
-      throw new Error("Ano inválido");
-    }
-    this.Year = value;
+  set year(value: number) {
+      this._year = value;
   }
 
   getMonthName(): string {
-    const months = [
-      "Janeiro",
-      "Fevereiro",
-      "Março",
-      "Abril",
-      "Maio",
-      "Junho",
-      "Julho",
-      "Agosto",
-      "Setembro",
-      "Outubro",
-      "Novembro",
-      "Dezembro",
-    ];
-    return months[this.Month - 1];
+      const months = [
+          'janeiro',
+          'fevereiro',
+          'março',
+          'abril',
+          'maio',
+          'junho',
+          'julho',
+          'agosto',
+          'setembro',
+          'outubro',
+          'novembro',
+          'dezembro',
+      ];
+
+      return months[this.month - 1];
   }
 
   isLeapYear(): boolean {
-    return (
-      (this.Year % 4 === 0 && this.Year % 100 !== 0) || this.Year % 400 === 0
-    );
+      return this._year % 4 === 0;
   }
 
   compare(date: Data): number {
-    const currentDateString = `${this.day}/${this._month}/${this._year}`;
 
-    const dateString = `${date.day}/${date._month}/${date._year}`;
+      const currentDateStr = `${this.year}-${this.month}-${this.day}`;
 
-    if (new Date(currentDateString) > new Date(dateString)) {
-      return 1;
-    }
-    if (new Date(currentDateString) < new Date(dateString)) {
+      const dateStr = `${date.year}-${date.month}-${date.day}`;
+
+      if (new Date(currentDateStr) > new Date(dateStr)) return 1;
+      if (new Date(currentDateStr) < new Date(dateStr)) return -1;
+
       return 0;
-    }
   }
 
   format(formatting: string): string {
-    const conditions: boolean[] = [
-      !formatting.match(/a{2,4}/g), // verifica se possui o ano na formatação
-      !formatting.match(/m{2}/g) && !formatting.match(/M{1}/g), // verifica se tem o mês na formatação
-      !formatting.match(/d{2}/g), // verifica se tem o dia na formatação
-    ];
 
-    if (conditions.every((cond) => cond)) {
-      throw new Error(`O formato passado é inválido: ${formatting}`);
-    }
+      const conditions: boolean[] = [
+          (!formatting.match(/a{2,4}/g)), // verifica se possui o ano na formatação
+          (!formatting.match(/m{2}/g) && !formatting.match(/M{1}/g)), // verifica se tem o mês na formatação
+          (!formatting.match(/d{2}/g)) // verifica se tem o dia na formatação
+      ];
 
-    const day = this.day > 9 ? this.day.toString() : `0${this.day.toString()}`;
-    const month =
-      this._month > 9 ? this._month.toString() : `0${this._month.toString()}`;
-    const year = this._year.toString();
+      if (conditions.every(cond => cond)) {
+          throw new Error(`O formato passado é inválido: ${formatting}`);
+      }
 
-    const dateFormatting = formatting
-      .replace("dd", day)
-      .replace("mm", month)
-      .replace("M", this.getMonthName())
-      .replace("aaaa", year)
-      .replace("aa", year.substr(-2));
+      const day = this.day > 9 ? this.day.toString() : `0${this.day.toString()}`;
+      const month = this.month > 9 ? this.month.toString() : `0${this.month.toString()}`;
+      const year = this.year.toString();
 
-    return dateFormatting;
+      const dateFormatting = formatting
+          .replace('dd', day)
+          .replace('mm', month)
+          .replace('M', this.getMonthName())
+          .replace('aaaa', year)
+          .replace('aa', year.substr(-2));
+
+      return dateFormatting;
   }
 
   private validateDate(day: number, month: number, year: number): boolean {
-    const dateStr = `${year}-${month}-${day}`;
+      const dateStr = `${year}-${month}-${day}`;
 
-    if (new Date(dateStr).getDate() !== day) return false;
-    return true;
+      if (new Date(dateStr).getDate() !== day)
+          return false;
+      return true
   }
+
 }
+
+// Para testar!
+
 const testDate = new Data(29, 1, 1989);
 
 console.log(testDate);
 console.log('Dia: ', testDate.day);
-console.log('Mês: ', testDate.Month);
+console.log('Mês: ', testDate.month);
 console.log('Mês por extenso: ', testDate.getMonthName());
-console.log('Ano: ', testDate.Year);
+console.log('Ano: ', testDate.year);
 console.log('É ano bissexto: ', testDate.isLeapYear() ? 'Sim' : 'Não');
 console.log(testDate.format('dd/mm/aaaa'));
 console.log(testDate.format('dd-mm-aaaa'));
