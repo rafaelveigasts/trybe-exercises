4ebb18f79278192559921dd1c185ade75d96976c
rafael veiga <rafaelveig@hotmail.com>
2022-03-07 21:44:37 -0300

    exercicio 3

--- bloco-27-poo-solid/dia-2-heranca-e-interfaces/Conteudos/Exercicios/index.ts
+++ bloco-27-poo-solid/dia-2-heranca-e-interfaces/Conteudos/Exercicios/index.ts
@@ -1,24 +1,17 @@
 // Para testar!
-// ,/index.ts
+// ./index.ts
 
-import Student from './Student';
+import Employee from './Employee';
 
-const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
-const lucas = new Student('Lucas Peixoto Salgueiro', new Date('2006/07/19'));
-const jessica = new Student('Jéssica Bianca Nunes', new Date('2004/06/06'));
-const tamires = new Student('Tamires Santos Bastos', new Date('2005/11/05'));
-const fernando = new Student('Fernando Gonçalves', new Date('2006/09/11'));
+const testInterfaceEmployee: Employee = {
+    registration: 'FNC1234567891011',
+    salary: 1200.00,
+    admissionDate: new Date(),
+    generateRegistration(): string {
+        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
 
-carolina.examsGrades = [25, 20, 25, 23];
-lucas.examsGrades = [25, 20, 25, 23];
-jessica.worksGrades = [50, 45];
-tamires.worksGrades = [47, 42];
+        return `FNC${randomStr}`;
+    }
+}
 
-console.log(carolina);
-console.log(lucas);
-console.log(jessica);
-console.log(tamires);
-console.log(fernando);
-
-// deve retornar erro
-// fernando.worksGrades = [45, 45, 45];
+console.log(testInterfaceEmployee);
