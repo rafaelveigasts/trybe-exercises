bb21e999df60048b36b51b8726f32d4edb3c7cb8
rafael veiga <rafaelveig@hotmail.com>
2022-03-07 21:43:32 -0300

    exercicio 1

---
+++ bloco-27-poo-solid/dia-2-heranca-e-interfaces/Conteudos/Exercicios/index.ts
@@ -0,0 +1,15 @@
+// Para testar!
+// ./index.ts
+
+import Person from './Person';
+
+const maria = new Person('Maria da Consolação', new Date('1980/01/25'));
+const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
+
+console.log(maria);
+console.log(luiza);
+
+// deve retornar erro
+// const invalidPersonName = new Person('An', new Date('2000/06/07'));
+// deve retornar erro
+// const invalidPersonAge = new Person('Ana Vitória', new Date('1900/06/07'));
