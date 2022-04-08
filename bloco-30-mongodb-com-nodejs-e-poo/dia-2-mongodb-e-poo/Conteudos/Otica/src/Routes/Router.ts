7d2d7ab3169951575476c5cc35d3bcfe42d1528b
Rafael <rafaelveig@hotmail.com>
2022-04-08 18:36:32 -0300

    routes

---
+++ bloco-30-mongodb-com-nodejs-e-poo/dia-2-mongodb-e-poo/Conteudos/Otica/src/Routes/Router.ts
@@ -0,0 +1,23 @@
+  // src/Routes/Router.ts
+
+  import { Router } from 'express';
+  import Controller from '../Controllers';
+
+  class CustomRouter<T> {
+    public router: Router;
+
+    constructor() {
+      this.router = Router();
+    }
+
+    public addRoute(
+      controller: Controller<T>,
+      route: string = controller.route,
+    ) {
+      this.router.get(route, controller.read);
+      this.router.get(`${route}/:id`, controller.readOne);
+      this.router.post(route, controller.create);
+    }
+  }
+
+  export default CustomRouter;
