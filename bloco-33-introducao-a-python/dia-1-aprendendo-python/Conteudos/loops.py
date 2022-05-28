7bc24d1b0dded6faeff5a934742977e50b38c98a
Rafael <rafaelveig@hotmail.com>
2022-05-28 10:00:50 -0300

    complemento

---
+++ bloco-33-introducao-a-python/dia-1-aprendendo-python/Conteudos/loops.py
@@ -0,0 +1,45 @@
+restaurants = [
+    {"name": "Restaurante A", "nota": 4.5},
+    {"name": "Restaurante B", "nota": 3.0},
+    {"name": "Restaurante C", "nota": 4.2},
+    {"name": "Restaurante D", "nota": 2.3},
+]
+
+filtered_restaurants = []
+min_rating = 3.0
+for restaurant in restaurants:
+    if restaurant["nota"] > min_rating:
+        filtered_restaurants.append(restaurant)
+print(filtered_restaurants)  # imprime a lista de restaurantes, sem o B e D
+
+min_rating = 3.0
+filtered_restaurants = [restaurant for restaurant in restaurants if restaurant["nota"] > min_rating]
+print(filtered_restaurants)  # imprime a lista de restaurantes, sem o B e D
+
+
+#sequencia fibonacci em python
+
+n = 10
+last, next = 0, 1
+while last < n:
+    print(last)
+    last, next = next, last + next
+
+
+languages = ['Python', 'Java', 'JavaScript']
+
+enumerate_prime = enumerate(languages)
+
+# converte um objeto enumerate em uma lista
+print(list(enumerate_prime))
+
+# Saída: [(0, 'Python'), (1, 'Java'), (2, 'JavaScript')]
+
+languages = ['Python', 'Java', 'JavaScript']
+
+enumerate_prime = enumerate(languages)
+
+# converte um objeto enumerate em uma lista
+print(list(enumerate_prime))
+
+# Saída: [(0, 'Python'), (1, 'Java'), (2, 'JavaScript')]
